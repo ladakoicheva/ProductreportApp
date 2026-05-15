1bf6b2110af1c8c38caa55c3fa2081684f1a2af1
Lada Koicheva <koichevalada@gmail.com>
2026-05-15 15:28:04 +0300

    feature:now adaptive for phones

--- src/components/SafeMap/SafeMap.jsx
+++ src/components/SafeMap/SafeMap.jsx
@@ -2,12 +2,12 @@
 
 export default function SafeMap({ mapUrl }) {
   return (
-    <div>
+    <div style={{ width: '100%' }}>
       <iframe
         src={mapUrl}
         width="100%"
-        height="450"
-        style={{ border: 0 }}
+        height="100%"
+        style={{ border: 0, width: '100%', minHeight: '260px', height: 'min(58vw, 450px)' }}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
